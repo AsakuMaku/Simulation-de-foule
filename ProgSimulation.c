12a96a0d3ffa47479ff669447480bfbcd2d8b050
Unknown <japonmangas@hotmail.fr>
2016-10-28 15:55:12 +0200

    Initiation de plan *erreur*

--- ProgSimulation.c
+++ ProgSimulation.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <time.h>
 #include "lib/libgraphique.h"
+#include "lib/lib_foule.c"
 
 int main(void)
 {
+int plan[80][60];
 ouvrir_fenetre(800, 600);
-
+charge_plan("data/plan.txt", plan);
 actualiser();
 
 attendre_clic();
